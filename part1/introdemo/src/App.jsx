93256a675bd19dd730f061c5e84971654d36067b
hosna <hosna@kth.se>
2025-05-26 19:58:29 +0200

    1.14*: anecdotes step 3

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -39,10 +39,35 @@ const Button = ({ handleClick, text }) => {
 };
 
 const App = () => {
-  // save clicks of each button to its own state
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const handleClick = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  };
+
+  const handleVote = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  };
+
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+
+  const maxVotes = Math.max(...votes);
+  const topAnecdoteIndex = votes.indexOf(maxVotes);
 
   return (
     <div>
@@ -51,6 +76,12 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text={"Neutral"} />
       <Button handleClick={() => setBad(bad + 1)} text={"Bad"} />
       <Statistics good={good} neutral={neutral} bad={bad} />
+      <h2>Anecdotes of the day!</h2>
+      <button onClick={handleVote}>Vote</button>
+      <button onClick={handleClick}> Next anecdotes</button>
+      <div> {anecdotes[selected]} </div>
+      <h3>Anecdotes with most votes</h3>
+      <div> {anecdotes[topAnecdoteIndex]} </div>
     </div>
   );
 };
