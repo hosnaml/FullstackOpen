db09bbf85aba78d2cc14eea8609837fa6e2fed19
hosna <hosna@kth.se>
2025-08-30 20:46:30 +0200

    Add totalLikes function and corresponding tests for blog list helper

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
   
-module.exports = {dummy}
-    
-  
+const totalLikes = (blogs) => {
+    if (blogs.length == 1){
+        return (blogs[0].likes)
+    }
+    if (blogs.length == 0){
+        return (0)
+    }
+    return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+
+module.exports = {dummy, totalLikes}
