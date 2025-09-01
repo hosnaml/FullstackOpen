82044a2a822a1fd41d6259642b3f4034b18d8e42
hosna <hosna@kth.se>
2025-09-01 18:10:20 +0200

    add PUT endpoint for updating blog posts
    - Implement PUT /api/blogs/:id endpoint using async/await
    - Support updating blog fields (primarily likes count)
    - Add proper error handling for invalid IDs and missing blogs
    - Return updated blog object with 200 status
    - Return 404 for non-existent blogs, 400 for validation errors

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -7,12 +7,51 @@ blogsRouter.get('/', (request, response) => {
     })
   })
   
-blogsRouter.post('/', (request, response) => {
-const blog = new Blog(request.body)
-
-blog.save().then((result) => {
-    response.status(201).json(result)
+blogsRouter.get('/:id', async (request, response) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    response.status(400).json({ error: error.message })
+  }
 })
+
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      request.params.id, 
+      request.body, 
+      { new: true, runValidators: true }
+    )
+    
+    if (updatedBlog) {
+      response.json(updatedBlog)
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    response.status(400).json({ error: error.message })
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch (error) {
+    response.status(400).json({ error: error.message })
+  }
 })
 
 module.exports = blogsRouter
