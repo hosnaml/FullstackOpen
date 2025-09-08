79e7d7914cac264f11052f83236719ae6037d1d5
hosna <hosna@kth.se>
2025-09-08 19:24:36 +0200

    Refactor blog API implementation to use separate router and database connection
    - Moved blog-related routes to a dedicated controller
    - Replaced Sequelize initialization with a database connection utility
    - Streamlined server startup process and improved code organization

--- part13/index.js
+++ part13/index.js
@@ -1,77 +1,20 @@
 require('dotenv').config()
-const { Sequelize, Model, DataTypes } = require('sequelize')
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: 'postgres' })
 const express = require('express')
 const app = express()
 
+const { PORT } = require('./utils/config')
+const { connectToDatabase } = require('./utils/db')
+
+const blogsRouter = require('./controllers/blogs')
+
 app.use(express.json())
-
-class Blog extends Model {}
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  author: {
-    type: DataTypes.TEXT,
-    allowNull: true
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  title: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  likes: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0
-  }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'blog',
-  tableName: 'blogs'
-})
-
-app.get('/api/blogs', async (req, res) => {
-  const blogs = await Blog.findAll()
-  res.json(blogs)
-})
-
-app.post('/api/blogs', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body)
-    res.json(blog)
-  } catch (error) {
-    return res.status(400).json({ error })
-  }
-})
-
-app.delete('/api/blogs/:id', async (req, res) => {
-  const id = Number(req.params.id)
-  if (!Number.isInteger(id)) return res.status(400).json({ error: 'Invalid id' })
-  const deletedCount = await Blog.destroy({ where: { id } })
-  if (deletedCount === 0) return res.status(404).end()
-  return res.status(204).end()
-})
+app.use('/api/blogs', blogsRouter)
 
 const start = async () => {
-  try {
-    await sequelize.authenticate()
-    const PORT = process.env.PORT || 3001
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`)
-    })
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-    process.exit(1)
-  }
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
 }
 
 start()
