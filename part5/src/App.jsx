92fae4bcf4a6a18de075e4648d01a6cfb67fdd0d
Hosna <hosna.m214@gmail.com>
2025-10-05 19:51:32 +0200

    Integrate Togglable components, fix username display, add blog sorting by likes, and implement refs for form control

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import LoginForm from './components/LoginForm'
 import loginService from './services/login'
 import BlogForm from './components/BlogForm'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [message, setMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const blogFormRef = useRef()
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -34,18 +39,20 @@ const App = () => {
       )
       blogService.setToken(user.token)
       setUser(user)
-      setMessage({ text: `Hello ${credentials.username}`, type: 'success' })
+      setMessage({ text: `Hello ${user.username}`, type: 'success' })
     } catch (exception) {
       setMessage({ text: 'Wrong credentials', type: 'error' })
     }
   }
 
   const handleLogout = () => {
-    setMessage({ text: `Goodbye ${user.name}`, type: 'success' })
+    setMessage({ text: `Goodbye ${user.username}`, type: 'success' })
     setUser(null)
     window.localStorage.removeItem('loggedBlogAppUser')
   }
+  
   const handleCreateBlog = async (blog) => {
+    blogFormRef.current.toggleVisibility()
     const newBlog = await blogService.create(blog)
     setBlogs(blogs.concat(newBlog))
     setMessage({ text: `A new blog "${newBlog.title}" by ${newBlog.author} added`, type: 'success' })
@@ -56,17 +63,29 @@ const App = () => {
       <h1>blogs</h1>
       <Notification message={message} setMessage={setMessage} />
       {!user ?
-        <LoginForm onLogin={handleLogin} />
+      <Togglable buttonLabel="Log in">
+          <LoginForm
+           onLogin={handleLogin}
+           username={username}
+           password={password}
+           setUsername={setUsername}
+           setPassword={setPassword}
+            />
+      </Togglable>
         :
         <div>
-          <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
-          {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />
+          <p> {user.username.charAt(0).toUpperCase() + user.username.slice(1)} logged in <button onClick={handleLogout}>logout</button></p>
+          {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map(blog =>
+            <Blog key={blog.id} blog={blog} setBlogs={setBlogs}/>
           )}
-          <BlogForm onCreate={handleCreateBlog} />
+           <Togglable buttonLabel="Click to create a new blog" ref={blogFormRef}>
+              <BlogForm onCreate={handleCreateBlog} />
+           </Togglable>
         </div>
         
-      }
+      } 
     </div>
   )
 }
