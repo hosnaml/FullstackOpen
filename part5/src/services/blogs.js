093c848a3311d2005a55e06b1d425a21f3c8009d
Hosna <hosna.m214@gmail.com>
2025-10-05 19:50:57 +0200

    Add authorization token to blog update requests

--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -20,7 +20,10 @@ const create = newObject => {
 }
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  const config = {
+    headers: { Authorization: token },
+  }
+  const request = axios.put(`${baseUrl}/${id}`, newObject, config)
   return request.then(response => response.data)
 }
 
