2277fcf308a3548f335a957dc856c7b5726ac0cf
Hosna <hosna.m214@gmail.com>
2025-10-29 23:49:10 +0100

    Fix: ensure anecdote.content is string; pass content string to create mutation

---
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import AnecdoteService from '../services/anecdotes'
+
+const AnecdoteForm = () => {
+
+  const queryClient = useQueryClient()
+
+  const newNoteMutation = useMutation({
+    mutationFn: AnecdoteService.createNew,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+  })
+
+  const onCreate = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newNoteMutation.mutate(content)
+  }
+
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name="anecdote" />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
